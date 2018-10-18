336344aa51e2b13987f7ba3a37625b13282429e7
mjrfornari <mjrfornari@gmail.com>
2018-10-18 18:14:08 -0300

    asd

--- server.js
+++ server.js
@@ -61,9 +61,12 @@ app.get('/clientes/:user', cors(corsOptions), function (req, res, next) {
         if (err)
             throw err;                        
 
-        db.query('SELECT pk_cli, cast(codigo_representada as varchar(20) character SET UTF8) codigo_representada,'+ 
-        'cast(razao_social as varchar(50) character SET UTF8) razao_social, cast(cnpj as varchar(14) character SET UTF8) cnpj,'+
-        'cast(fone1 as varchar(20) character SET UTF8) fone1 '+
+        db.query('SELECT pk_cli, trim(cast(nome_fantasia as varchar(50) character SET UTF8)) NOME_FANTASIA,'+ 
+        'trim(cast(razao_social as varchar(50) character SET UTF8)) razao_social, trim(cast(cnpj as varchar(14) character SET UTF8)) cnpj,'+
+        'trim(cast(fone1 as varchar(20) character SET UTF8)) fone1, trim(cast(INSCRICAO_ESTADUAL as varchar(20) character SET UTF8)) INSCRICAO_ESTADUAL, trim(cast(INSCRICAO_MUNICIPAL as varchar(20) character SET UTF8)) INSCRICAO_MUNICIPAL, '+
+        'trim(cast(SUFRAMA as varchar(20) character SET UTF8)) SUFRAMA, trim(cast(ENDERECO as varchar(100) character SET UTF8)) ENDERECO, trim(cast(NUMERO as varchar(20) character SET UTF8)) NUMERO, '+
+        'trim(cast(BAIRRO as varchar(50) character SET UTF8)) BAIRRO, trim(cast(CEP as char(8) character SET UTF8)) CEP, trim(cast(COMPLEMENTO as varchar(20) character SET UTF8)) COMPLEMENTO, '+
+        'FK_CID, DDD1, DDD2, trim(cast(FONE2 as varchar(20) character SET UTF8)) FONE2, trim(cast(EMAIL as varchar(40) character SET UTF8)) EMAIL, trim(cast(EMAIL_FINANCEIRO as varchar(100) character SET UTF8)) EMAIL_FINANCEIRO '+
         'FROM clientes WHERE FK_VEN='+db.escape(req.params['user'])+' or FK_VEN2='+db.escape(req.params['user']), function(err, result) {
             // IMPORTANT: close the connection
       
@@ -82,21 +85,73 @@ app.get('/pedidos/:user', cors(corsOptions), function (req, res, next) {
             throw err;                        
 
         let sql = 'select PED.PK_PED, PED.NRO_MACROPECAS, PED.FK_CLI, CLI.RAZAO_SOCIAL, PED.FK_CPG, CPG.NOME NOMECPG, '+
-                'PED.DATA, PED.VALOR_CALCULADO, PED.VALOR_INFORMADO, cast(PED.OBSERVACAO as varchar(5000) character SET UTF8), '+
-                'cast(PED.ORCAMENTO as char(1) character SET UTF8), PED.DATA_ENVIO, PED.NUMPED, PED.NUMORC, '+
-                'cast(PED.ENVIADO as char(1) character SET UTF8), cast(PED.IMPORTACAO as char(1) character SET UTF8),'+
-                'cast(PED.STATUS as char(1) character SET UTF8), cast(PED.WEB as char(1) character SET UTF8),'+
+                'PED.DATA, PED.VALOR_CALCULADO, PED.VALOR_INFORMADO, trim(cast(PED.OBSERVACAO as varchar(5000) character SET UTF8)), '+
+                'trim(cast(PED.ORCAMENTO as char(1) character SET UTF8)), cast(PED.DATA_ENVIO as date) DATA_ENVIO, PED.NUMPED, PED.NUMORC, '+
+                'trim(cast(PED.ENVIADO as char(1) character SET UTF8)), trim(cast(PED.IMPORTACAO as char(1) character SET UTF8)),'+
+                'trim(cast(PED.STATUS as char(1) character SET UTF8)), trim(cast(PED.WEB as char(1) character SET UTF8)),'+
                 'PED.DESCONTO1, PED.DESCONTO2, PED.DESCONTO3 '+
                 'from PEDIDOS_VENDA PED '+
                 'join CLIENTES CLI on CLI.PK_CLI = PED.FK_CLI '+
                 'join COND_PAG CPG on CPG.PK_CPG = PED.FK_CPG '+
                 'WHERE PED.FK_VEN='+db.escape(req.params['user']);
-        console.log(sql)
+        // console.log(sql)
 
         db.query(sql, function(err, result) {
             // IMPORTANT: close the connection
       
-            console.log(result)
+            // console.log(result)
+            db.detach();
+            res.json(result)
+        });
+
+    });
+})
+
+function campoVarChar (campo, tamanho){
+    let x ='trim(cast('+campo+' as varchar('+tamanho+') character SET UTF8)) '+campo
+    return x
+}
+
+function campoDate (campo){
+    let x ='cast('+campo+' as date) '+campo
+    return x
+}
+
+app.get('/produtos', cors(corsOptions), function (req, res, next) {
+    
+    Firebird.attach(optionsfb, function(err, db) {
+        if (err)
+            throw err;                        
+
+        let sql = 'select PK_PRO, trim(cast(CODIGO_REPRESENTADA as varchar(20) character SET UTF8)) CODIGO_REPRESENTADA, trim(cast(NOME_REPRESENTADA as varchar(100) character SET UTF8)) NOME_REPRESENTADA, trim(cast(CLASSIFICACAO_FISCAL as varchar(10) character SET UTF8)) CLASSIFICACAO_FISCAL, '+
+        'trim(cast(CODIGO_BARRAS as varchar(20) character SET UTF8)) CODIGO_BARRAS, IPI, PRECO_VENDA_LISTA, PRECO_REGIAO_1, PRECO_REGIAO_2, PRECO_REGIAO_3, PRECO_REGIAO_4, '+
+        'PRECO_VENDA_PROMO, PRECO_PROM_REGIAO_1, PRECO_PROM_REGIAO_2, PRECO_PROM_REGIAO_3,PRECO_PROM_REGIAO_4, PERC_DESC_PROMO, '+ 
+        campoDate('DATA_VALID_PROMO')+', trim(cast(OBS_PROMOCIONAL as varchar(200) character SET UTF8)) OBS_PROMOCIONAL, '+campoDate('DATA_ATUALIZACAO_PRECOS')+' from PRODUTOS where ATIVO='+db.escape('S')+' order by NOME_REPRESENTADA, CODIGO_REPRESENTADA ';
+        
+
+        db.query(sql, function(err, result) {
+            // IMPORTANT: close the connection
+      
+
+            db.detach();
+            res.json(result)
+        });
+
+    });
+})
+
+app.get('/sticms', cors(corsOptions), function (req, res, next) {
+    
+    Firebird.attach(optionsfb, function(err, db) {
+        if (err)
+            throw err;                        
+
+        let sql = 'select PK_STI, '+campoVarChar('ORIGEM',2)+', FK_PRO, FK_ESTDESTINO, '+campoVarChar('SIMPLES_NACIONAL', 1)+', PERCENTUAL_ST, FK_ESTORIGEM from ST_ICMS'
+
+        db.query(sql, function(err, result) {
+            // IMPORTANT: close the connection
+      
+            // console.log(result)
             db.detach();
             res.json(result)
         });
@@ -110,17 +165,17 @@ app.get('/itepedidos/:pedido', cors(corsOptions), function (req, res, next) {
         if (err)
             throw err;                        
 
-        let sql = 'select IPE.PK_IPE, IPE.FK_PRO, cast(PRO.CODIGO_MACROPECAS as varchar(20) character SET UTF8) CODIGOPRO, IPE.QUANTIDADE, IPE.VALOR, IPE.DESCONTO1, IPE.DESCONTO2, IPE.DESCONTO3, '+
-                'cast(IPE.OBSERVACAO as varchar(100) character SET UTF8), IPE.CONTROLE, IPE.PERC_STICMS, IPE.VALOR_STICMS '+
+        let sql = 'select IPE.PK_IPE, IPE.FK_PRO, trim(cast(PRO.CODIGO_MACROPECAS as varchar(20) character SET UTF8)) CODIGOPRO, IPE.QUANTIDADE, IPE.VALOR, IPE.DESCONTO1, IPE.DESCONTO2, IPE.DESCONTO3, '+
+                'trim(cast(IPE.OBSERVACAO as varchar(100) character SET UTF8)), IPE.CONTROLE, IPE.PERC_STICMS, IPE.VALOR_STICMS, (IPE.QUANTIDADE*IPE.VALOR*(IPE.DESCONTO1/100)) as TOTAL '+
                 'from ITENS_PED_VENDA IPE '+
                 'join PRODUTOS PRO on PRO.PK_PRO = IPE.FK_PRO '+
                 'WHERE IPE.FK_PED='+db.escape(req.params['pedido']);
-        console.log(sql)
+        // console.log(sql)
 
         db.query(sql, function(err, result) {
             // IMPORTANT: close the connection
       
-            console.log(result)
+            // console.log(result)
             db.detach();
             res.json(result)
         });
@@ -135,16 +190,15 @@ app.get('/cpg', cors(corsOptions), function (req, res, next) {
         if (err)
             throw err;                        
 
-        let sql = 'select CPG.PK_CPG, cast(CPG.NOME as varchar(50) character SET UTF8) NOME, CPG.DESCONTO, cast(CPG.CODIGO_REPRESENTADA as varchar(50) character SET UTF8) CODIGO_REPRESENTADA, '+
-                'cast(CPG.BLOQ_FIN as char(1) character SET UTF8) BLOQ_FIN '+
-                'from COND_PAG CPG '+
-                'WHERE CPG.INATIVO='+db.escape('N');
-        console.log(sql)
+        let sql = 'select CPG.PK_CPG, trim(cast(CPG.NOME as varchar(50) character SET UTF8)) NOME, CPG.DESCONTO, trim(cast(CPG.CODIGO_REPRESENTADA as varchar(50) character SET UTF8)) CODIGO_REPRESENTADA, '+
+                ' trim(cast(CPG.BLOQ_FIN as char(1) character SET UTF8)) BLOQ_FIN, trim(cast(CPG.INATIVO as char(1) character SET UTF8)) INATIVO '+
+                'from COND_PAG CPG ';
+        // console.log(sql)
 
         db.query(sql, function(err, result) {
             // IMPORTANT: close the connection
       
-            console.log(result)
+            // console.log(result)
             db.detach();
             res.json(result)
         });
@@ -163,7 +217,7 @@ app.get('/gerapk/:nomepk', cors(corsOptions), function (req, res, next) {
             // console.log(result)
             db.query('select valor from controle where campo = '+db.escape(req.params['nomepk']), function(err, result) {
                 // IMPORTANT: close the connection
-                console.log(result)
+                // console.log(result)
                 res.json(result)
                 db.detach();
             });
@@ -229,7 +283,7 @@ app.get('/produtos', cors(corsOptions), function (req, res, next) {
         if (err)
             throw err;
         // db = DATABASE                         
-        let sql = 'SELECT pk_pro, cast(codigo_representada as varchar(20) character SET UTF8) codigo_representada, cast(codigo_macropecas as varchar(20) character SET UTF8) codigo_macropecas,cast(nome_macropecas as varchar(100) character SET UTF8) nome_macropecas '+
+        let sql = 'SELECT pk_pro, trim(cast(codigo_representada as varchar(20) character SET UTF8)) codigo_representada, trim(cast(codigo_macropecas as varchar(20) character SET UTF8)) codigo_macropecas,trim(cast(nome_macropecas as varchar(100) character SET UTF8)) nome_macropecas '+
         'FROM produtos where ativo='+db.escape("S");
         console.log(sql)
         db.query(sql, function(err, result) {
@@ -250,9 +304,9 @@ app.get('/asdd', cors(corsOptions), function (req, res, next) {
             throw err;
         // db = DATABASE                         
 
-        db.query('SELECT pk_cli, cast(codigo_representada as varchar(20) character SET UTF8) codigo_representada,'+ 
-        'cast(razao_social as varchar(50) character SET UTF8) razao_social, cast(cnpj as varchar(14) character SET UTF8) cnpj,'+
-        'cast(fone1 as varchar(20) character SET UTF8) fone1 '+
+        db.query('SELECT pk_cli, trim(cast(codigo_representada as varchar(20) character SET UTF8)) codigo_representada,'+ 
+        'trim(cast(razao_social as varchar(50) character SET UTF8)) razao_social, trim(cast(cnpj as varchar(14) character SET UTF8)) cnpj,'+
+        'trim(cast(fone1 as varchar(20) character SET UTF8)) fone1 '+
         'FROM clientes', function(err, result) {
             // IMPORTANT: close the connection
       
@@ -271,9 +325,9 @@ app.get('/asd', cors(corsOptions), function (req, res, next) {
             throw err;
         // db = DATABASE                         
 
-        db.query('SELECT pk_cli, cast(codigo_representada as varchar(20) character SET UTF8) codigo_representada,'+ 
-        'cast(razao_social as varchar(50) character SET UTF8) razao_social, cast(cnpj as varchar(14) character SET UTF8) cnpj,'+
-        'cast(fone1 as varchar(20) character SET UTF8) fone1 '+
+        db.query('SELECT pk_cli, trim(cast(codigo_representada as varchar(20) character SET UTF8)) codigo_representada,'+ 
+        'trim(cast(razao_social as varchar(50) character SET UTF8)) razao_social, trim(cast(cnpj as varchar(14) character SET UTF8)) cnpj,'+
+        'trim(cast(fone1 as varchar(20) character SET UTF8)) fone1 '+
         'FROM clientes', function(err, result) {
             // IMPORTANT: close the connection
       
